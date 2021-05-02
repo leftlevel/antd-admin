318565dfaa0584da203bf7aa82c9f28bceef34f4
Xia, Mao-cong <69415869+leftlevel@users.noreply.github.com>
2021-05-02 22:18:59 +0800

    add: 国际化配置

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <template>
-  <router-view />
+  <a-config-provider :locale="locale">
+    <router-view />
+  </a-config-provider>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+// 国际化处理（en_GB 为英文）
+import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
+
+export default defineComponent({
+  setup() {
+    return {
+      locale: zhCN
+    }
+  },
+})
+</script>
+
 
 <style lang="less">
 </style>
